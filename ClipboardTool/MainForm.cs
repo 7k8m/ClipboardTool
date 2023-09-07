5c8ca5d8ff362995b4fe09af6310728a7261b58c
Tomohito Nakayama <tmht@7k8m.me>
2023-09-07 21:18:50 +0900

    Added code comment with GitHub Copilot

--- ClipboardTool/MainForm.cs
+++ ClipboardTool/MainForm.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 
 namespace ClipboardTool
 {
+    /// <summary>
+    /// Main Form of the Clipboard Tool.
+    /// </summary>
     public partial class MainForm : Form
     {
         public MainForm()
@@ -17,20 +20,32 @@ namespace ClipboardTool
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Read From Clipboard Button Clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void _btn_readFromClipboard_Click(object sender, EventArgs e)
         {
             _txtBox_clipboardContent.Text = 
                 ReadAsString(Clipboard.GetDataObject());
         }
 
+        /// <summary>
+        /// Read the clipboard data as string.
+        /// </summary>
+        /// <param name="clipboardData"></param>
+        /// <returns></returns>
         private static String ReadAsString(IDataObject clipboardData)
         {
             if (clipboardData.GetDataPresent(DataFormats.CommaSeparatedValue))
             {
+                //CSV
                 return (String) clipboardData.GetData(DataFormats.UnicodeText);
             }
             else if (clipboardData.GetDataPresent(DataFormats.Html))
             {
+                //HTML
                 return (String) clipboardData.GetData(DataFormats.Html);
             }
             else if(clipboardData.GetDataPresent(DataFormats.UnicodeText))
